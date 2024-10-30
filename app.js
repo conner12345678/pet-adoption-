a39ae8d23a8805cb2bc4b0aae1c6262d71d098f7
conner12345678 <cehly065@west-mec.org>
2024-10-30 15:20:45 -0700

    edited stuff ig

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const adopt = require('./routes/users');
 const connectDB = require('./db/connect');
 const port = process.env.PORT || 5000
+const path = require('path')
 
 const notFound = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
@@ -12,18 +13,24 @@ app.use(express.static('./public'));
 app.use(express.json());
 
 app.use('/api/v1/adopt', adopt);
-app.use(notFound);
-app.use(errorHandlerMiddleware);
 
 //sign in/up
 app.get('/', (req,res) => {
-    res.send('hello')
+    res.sendFile(path.join(__dirname, 'public/sign_in.html'))
 })
+// app.get('/signup', (req,res) => {
+//     res.sendFile(path.join(__dirname, 'public/sign_up.html'))
+// })
+
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+
 
 const serverInit = async () => {
     try{
         await connectDB();
-        app.listen(3000, () => console.log(`listening on http:/${port}`))
+        app.listen(3000, () => console.log(`listening on http://localhost:${port}`))
     }catch(error){
         console.error('Error starting server:', error)
     }   
